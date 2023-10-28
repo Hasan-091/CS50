98443d6b4ee56d0da1890698ebc8dc086bf8b2b9
Abdullah Ibnah Hasan <147977301+Hasan-091@users.noreply.github.com>
2023-10-29 04:50:55 +0600

    Add files via upload

---
+++ CS50/mario-less/mario.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height, row, column, blank;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    for (row = 0; row < height; row++)
+    {
+        for (blank = 0; blank < height - row - 1; blank++)
+        {
+            printf(" ");
+        }
+        for (column = 0; column <= row; column++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
